3d2f4a50ce356823d501705101914f9f1f88f4c8
Gang Li <leemagpie@gmail.com>
2014-06-28 20:57:54 -0400

    add phrase mode

--- alignment.py
+++ alignment.py
@@ -2,6 +2,10 @@
 import operator
 
 class Alignment(object):
+
+    SCORE_UNIFORM = 1
+    SCORE_LENGTH = 2    
+
     def __init__(self,seqa,seqb):
         self.seqa = seqa
         self.seqb = seqb
@@ -11,9 +15,11 @@ class Alignment(object):
         self.scoreSub = -100
         self.scoreDel = -2
         self.scoreIns = -1
-        self.separator = '|'
+        self.separator = u'|'
+        self.mode = Alignment.SCORE_UNIFORM
 
-    def set_score(self,scoreNull=None,scoreSub=None,scoreDel=None,scoreIns=None):
+    def set_score(self,scoreNull=None,scoreSub=None,scoreDel=None,scoreIns=None,
+                  mode=None):
         if scoreNull is not None:
             self.scoreNull = scoreNull
         if scoreSub is not None:
@@ -22,18 +28,28 @@ class Alignment(object):
             self.scoreDel = scoreDel
         if scoreIns is not None:
             self.scoreIns = scoreIns
+        if mode is not None:
+            self.mode = mode
 
     def match(self,a,b):
-        if a == b:
+        if a == b and self.mode == Alignment.SCORE_UNIFORM:
             return self.scoreNull
-        else:
+        elif self.mode == Alignment.SCORE_UNIFORM:
             return self.scoreSub
-    
+        elif a == b:
+            return self.scoreNull*len(a)
+        else:
+            return self.scoreSub*len(a)
+
     def delete(self,a):
-        return self.scoreDel
+        if self.mode == Alignment.SCORE_UNIFORM:
+            return self.scoreDel
+        return self.scoreDel*len(a)
 
     def insert(self,a):
-        return self.scoreIns
+        if self.mode == Alignment.SCORE_UNIFORM:
+            return self.scoreIns
+        return self.scoreIns*len(a)
 
     def score(self,Z,W):
         score = 0
@@ -80,54 +96,59 @@ class Needleman(Alignment):
         lenb = self.lenb
         
         for i in xrange(1,lena+1):
-            self.matrix[i][0] = self.scoreDel * i
+            self.matrix[i][0] = self.delete(seqa[i-1]) + self.matrix[i-1][0]
         
         for i in xrange(1,lenb+1):
-            self.matrix[0][i] = self.scoreIns * i
+            self.matrix[0][i] = self.insert(seqb[i-1]) + self.matrix[0][i-1]
 
         for i in xrange(1,lena+1):
             for j in xrange(1,lenb+1):
+                '''
+                Note that rows = lena+1, cols = lenb+1
+                '''
                 scoreSub = self.matrix[i-1][j-1] + self.match(seqa[i-1],seqb[j-1])
                 scoreDel = self.matrix[i-1][j] + self.delete(seqa[i-1])
                 scoreIns = self.matrix[i][j-1] + self.insert(seqb[j-1])                
                 self.matrix[i][j] = max(scoreSub,scoreDel,scoreIns)
 
     def backtrack(self):
-        Z = ''
-        W = ''
-        seqa = self.seqa
-        seqb = self.seqb
-        i = self.lena
-        j = self.lenb
+        alignSeqa,alignSeqb = [],[]
+        seqa,seqb = self.seqa,self.seqb
+        i,j = self.lena,self.lenb
         mat = self.matrix
-        
+
         while i > 0 or j > 0:
             if (i > 0 and j > 0 and
                 mat[i][j] == mat[i-1][j-1] + self.match(seqa[i-1],seqb[j-1])):
-
-                Z = seqa[i-1] + Z
-                W = seqb[j-1] + W
+                alignSeqa.insert(0,seqa[i-1])
+                alignSeqb.insert(0,seqb[j-1])
                 i -= 1
                 j -= 1
 
             elif j > 0 and mat[i][j] == mat[i][j-1] + self.insert(seqb[j-1]):
-                Z = self.separator + Z
-                W = seqb[j-1] + W
-                j -= 1                
+                alignSeqa.insert(0,self.separator*len(seqb[j-1]))
+                alignSeqb.insert(0,seqb[j-1])
+                j -= 1       
 
             elif i > 0 and mat[i][j] == mat[i-1][j] + self.delete(seqa[i-1]):
-                Z = seqa[i-1] + Z
-                W = self.separator + W
-                i -= 1            
-
-        return Z,W
+                alignSeqa.insert(0,seqa[i-1])
+                alignSeqb.insert(0,self.separator*len(seqa[i-1]))
+                i -= 1
+            else:
+                print seqa
+                print seqb
+                print alignSeqa
+                print alignSeqb
+                print mat
+                raise Exception('backtrack error',i,j,seqa[i-2:i+1],seqb[j-2:j+1])
+                pass
+        
+        return alignSeqa,alignSeqb
     
     def align(self):
         self.init_matrix()
         self.compute_matrix()
         return self.backtrack()
-
-
 
 class Hirschberg(Alignment):
     def __init__(self,*args):
@@ -143,7 +164,7 @@ class Hirschberg(Alignment):
             preRow[j] = preRow[j-1] + self.insert(seqb[j-1])
         
         for i in xrange(1,lena+1):
-            curRow[0] = i * self.delete(seqa[i-1])
+            curRow[0] = self.delete(seqa[i-1]) + preRow[0]
             for j in xrange(1,lenb+1):
                 scoreSub = preRow[j-1] + self.match(seqa[i-1],seqb[j-1])
                 scoreDel = preRow[j] + self.delete(seqa[i-1])
@@ -156,19 +177,17 @@ class Hirschberg(Alignment):
         return preRow
 
     def align_rec(self,seqa,seqb):
-        Z = ''
-        W = ''
-        lena = len(seqa)
-        lenb = len(seqb)
+        Z,W = [],[]
+        lena,lenb = len(seqa),len(seqb)
         
         if lena == 0:
             for i in xrange(lenb):
-                Z += self.separator
-                W += seqb[i]
+                Z.append(self.separator*len(seqb[i]))
+                W.append(seqb[i])
         elif lenb == 0:
             for i in xrange(lena):
-                Z += seqa[i]
-                W += self.separator
+                Z.append(seqa[i])
+                W.append(self.separator*len(seqa[i]))
 
         elif len(seqa) == 1:
             needleman = Needleman(seqa,seqb)
